f5ff5e00898fd37d0cf49742d1084e4501ecff2e
ranalav <lavkumarsinh.r@webcodegenie.com>
2023-06-09 18:30:19 +0530

    project intialized

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as Highcharts from 'highcharts';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,71 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'highchart';
+  Highcharts = Highcharts;
+
+  chartOptions:Highcharts.Options = {
+    chart: {
+      animation: false
+    },
+
+    title: {
+      text: "Highcharts draggable points demo"
+    },
+
+    xAxis: {
+      categories: [
+        "Jan",
+        "Feb",
+        "Mar",
+        "Apr",
+        "May",
+        "Jun",
+        "Jul",
+        "Aug",
+        "Sep",
+        "Oct",
+        "Nov",
+        "Dec"
+      ]
+    },
+
+    yAxis: {
+      softMin: 0,
+      softMax: 100
+    },
+
+    plotOptions: {
+      series: {
+        stickyTracking: false,
+        dragDrop: {
+          draggableY: true
+        },
+        point: {
+          events: {
+            drag: function (e:any) {
+              console.log("e ==> ", e);
+            },
+            // drop: function () {
+            // }
+          }
+        }
+      },
+      column: {
+        stacking: "normal",
+        minPointLength: 2
+      },
+      line: {
+        cursor: "ns-resize"
+      }
+    },
+
+    tooltip: {
+      valueDecimals: 2
+    },
+
+    series: [{
+      data: [1, 50, 3],
+      type: 'line'
+    }]
+  };
 }
